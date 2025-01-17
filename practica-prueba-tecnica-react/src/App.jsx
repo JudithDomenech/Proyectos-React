301a4daf012256d7879fa68d451bab0c4808f2db
Judith Domènech <judith.dp.3@gmail.com>
2025-01-17 10:18:41 +0100

    practica de `rueba tecnica

---
+++ practica-prueba-tecnica-react/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import { useCatImage } from "./Hooks/useCatImage.js";
+import { useCatFact } from "./Hooks/useCatFact.js";
+
+export function App() {
+  const { fact, refreshFact } = useCatFact();
+  const { imageUrl } = useCatImage({ fact });
+
+  const handleClick = async () => {
+    refreshFact();
+  };
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+
+      <button onClick={handleClick}>Get new fact</button>
+
+      {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={`Image extracted using the first three words for ${fact}`}
+        />
+      )}
+    </main>
+  );
+}
